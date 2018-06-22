0d21f8d1281473d1e7e0bebacbe628d23438e8e5
Francois Levesque <info@cumuluswebdesign.com>
2018-06-22 17:06:22 -0400

    Update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -47,6 +47,11 @@
                                 <i class="mdi mdi-twitter"></i>
                             </div>
                         </a>
+                        <a href="https://github.com/Witify">
+                            <div class="icon">
+                                <i class="mdi mdi-github-circle"></i>
+                            </div>
+                        </a>
                         <a href="https://dribbble.com/francoislevesque">
                             <div class="icon">
                                 <i class="mdi mdi-dribbble"></i>
