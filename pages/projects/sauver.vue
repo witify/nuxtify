72512eae82d2076b3112374ff06a007618cee8c0
= <patrick.vigeant@live.ca>
2018-07-18 11:44:08 -0400

    more text

--- pages/projects/sauver.vue
+++ pages/projects/sauver.vue
@@ -34,26 +34,26 @@
                                 </a>
                             </div>
                             <div class="project-summary-item">
-                                <h2 class="heading">Type</h2>
-                                <p class="is-medium">Application sur mesure</p>
-                                <p class="is-medium">Autopartage</p>
-                                <p class="is-medium">Gestion de flotte</p>
-                                <p class="is-medium">Environnement</p>
+                                <h2 class="heading">{{ $t('project.general.type') }}</h2>
+                                <p class="is-medium">{{ $t('project.general.custom-app') }}</p>
+                                <p class="is-medium">{{ $t('project.sauver.carsharing') }}</p>
+                                <p class="is-medium">{{ $t('project.sauver.fleet-management') }}</p>
+                                <p class="is-medium">{{ $t('project.sauver.environment') }}</p>
                             </div>
                             <div class="project-summary-item">
                                 <h2 class="heading">Services</h2>
-                                <p class="is-medium">Développement logiciel</p>
-                                <p class="is-medium">Conception de l'expérience utilisateur</p>
-                                <p class="is-medium">Gestion de flotte en temps réel</p>
+                                <p class="is-medium">{{ $t('project.general.software-dev') }}</p>
+                                <p class="is-medium">{{ $t('project.general.ux-dev') }}</p>
+                                <p class="is-medium">{{ $t('project.sauver.real-time-fleet-management') }}</p>
                             </div>
                         </div>
                     </div>
                     <div class="column">
                         <p class="is-lead mb-20">
-                        L'autopartage de voitures électriques est de plus en plus populaire dans les municipalités. Cependant, sans système de gestion en place, l'autopartage devient difficile et peu efficace. 
+                        {{ $t('project.sauver.header-1') }} 
                         </p>
                         <p>
-                        Afin de rentabiliser leur flotte, YHC Environnement a établi divers partenariats a travers les municipalités québécoises afin de répandre le système d’autopartage, en plus de favoriser une approche verte avec les voitures électriques. Ils nous ont ensuite approché pour bâtir leur plateforme web d'autopartage.
+                        {{ $t('project.sauver.subheader-1') }}
                         </p>
                     </div>
                 </div>
@@ -74,11 +74,11 @@
 
                 <div class="columns is-variable is-8">
                     <div class="column">
-                        <p class="is-medium">Le client a dû s’équiper d’experts de divers domaines afin de créer une offre de service complète pour ses partenaires. Witify devait donc collaborer avec une firme d’ingénierie et prendre en charge l’aspect logiciel du projet.</p>
+                        <p class="is-medium">{{ $t('project.sauver.approach-1') }}</p>
                     </div>
                     <div class="column">
                         <p class="is-medium">
-                        Le développement du produit web a été laissé sous l’expertise de l’équipe Witify afin de favoriser un produit facile d’utilisation pour ses divers utilisateurs.
+                        {{ $t('project.sauver.approach-2') }}
                         </p>
                     </div>
                 </div>
@@ -94,8 +94,8 @@
         <section class="hero is-quote">
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <h2 class="title">
-                    Un <strong>algorithme sophistiqué</strong> a été développé afin de prendre en compte autant les besoins de <strong>sécurité</strong>, de <strong>convenance</strong> et de <strong>logistique de gestion</strong>. Il analyse, réagit et effectue des actions en <strong>temps réel</strong> afin <strong>d'automatiser</strong> l'autopartage dans les municipalités.
+                    <h2 class="title" v-html="$t('project.sauver.header-2')">
+                    
                     </h2>
                 </div>
             </div>
@@ -104,8 +104,8 @@
         <section class="diptych is-border-bottom">
             <div class="container">
                 <div class="diptych-body">
-                    <h3 class="title">Gestion de flotte en temps réel</h3>
-                    <p class="is-medium">Initialement, l’équipe Witify a dû cibler les outils nécessaires afin de monitorer en temps réel l’ensemble des voitures pour chacune des municipalités. Les développeurs devaient pouvoir communiquer avec les divers véhicules, grâce à une API fournie par la firme d’ingénierie partenaire. Un algorithme sophistiqué a été développé afin de prendre en compte autant les besoins de sécurité, de convenance et de logistique de gestion. Il analyse, réagit et effectue des actions en temps réel afin d'automatiser l'autopartage 24 heures sur 24.</p>
+                    <h3 class="title">{{ $t('project.sauver.real-time-fleet-management') }}</h3>
+                    <p class="is-medium">{{ $t('project.sauver.paragraph-1') }}</p>
                 </div>
             </div>
             <div class="diptych-image pt-80">
@@ -116,8 +116,8 @@
         <section class="diptych is-border-bottom is-right">
             <div class="container">
                 <div class="diptych-body">
-                    <h3 class="title">Interface de réservation</h3>
-                    <p class="is-medium">L’équipe a confectionné une plateforme sur laquelle les utilisateurs peuvent réserver un véhicule pendant une plage plage horaire qui leur convienne. Les administrateurs, de leur côté, peuvent ajouter et modifier un large éventail de paramètres reliés à leur ville (véhicules, plages horaires, jours fériés, autorisations, etc.)</p>
+                    <h3 class="title">{{ $t('project.sauver.booking-interface') }}</h3>
+                    <p class="is-medium">{{ $t('project.sauver.paragraph-2') }}</p>
                 </div>
             </div>
             <div class="diptych-image pt-80">
@@ -128,7 +128,7 @@
         <section class="section is-medium">
             <div class="container">
                 <h2 class="title is-size-2">{{ $t('project.general.results') }}</h2>
-                <p class="is-medium mb-50">En tant que <strong>1er projet du genre au Canada</strong>, SAUVéR finit sa phase pilote avec <strong>7 municipalités à travers le Québec</strong>. Lancé en ligne en début janvier 2017, le projet a récolté beaucoup de traction.</p>
+                <p class="is-medium mb-50" v-html="$t('project.sauver.results')"></p>
 
                 <div class="columns is-multiline is-mobile is-clients mb-60">
                     <div class="column is-half-mobile">
@@ -156,15 +156,15 @@
                 <div class="columns">
                     <div class="column">
                         <div class="is-size-2 has-text-weight-semibold">10+</div>
-                        <span class="has-text-grey is-size-5">véhicules enregistrés</span>
+                        <span class="has-text-grey is-size-5">{{ $t('project.sauver.registered-vehicles') }}</span>
                     </div>
                     <div class="column">
                         <div class="is-size-2 has-text-weight-semibold">100+</div>
-                        <span class="has-text-grey is-size-5">utilisateurs actifs</span>
+                        <span class="has-text-grey is-size-5">{{ $t('project.sauver.active-users') }}</span>
                     </div>
                     <div class="column">
                         <div class="is-size-2 has-text-weight-semibold">1500+</div>
-                        <span class="has-text-grey is-size-5">réservations</span>
+                        <span class="has-text-grey is-size-5">{{ $t('project.sauver.bookings') }}</span>
                     </div>
                 </div>
             </div>
--- pages/projects/start-alberta.vue
+++ pages/projects/start-alberta.vue
@@ -34,21 +34,21 @@
                                 </a>
                             </div>
                             <div class="project-summary-item">
-                                <h2 class="heading">Type</h2>
-                                <p class="is-medium">Application sur mesure</p>
-                                <p class="is-medium">Réseau social</p>
-                                <p class="is-medium">Affaires</p>
+                                <h2 class="heading">{{ $t('project.general.type') }}</h2>
+                                <p class="is-medium">{{ $t('project.general.custom-app') }}</p>
+                                <p class="is-medium">{{ $t('project.general.social-network') }}</p>
+                                <p class="is-medium">{{ $t('project.general.business') }}</p>
                             </div>
                             <div class="project-summary-item">
-                                <h2 class="heading">Services</h2>
-                                <p class="is-medium">Développement logiciel</p>
-                                <p class="is-medium">Conception de l'expérience utilisateur</p>
+                                <h2 class="heading">{{ $t('project.general.services') }}</h2>
+                                <p class="is-medium">{{ $t('project.general.software-dev') }}</p>
+                                <p class="is-medium">{{ $t('project.general.ux-dev') }}</p>
                             </div>
                         </div>
                     </div>
                     <div class="column">
-                        <p class="is-lead mb-20">L’économie florissante de l’Alberta, impliquant 4,3 millions de résidents, presque 20 milliers nouvelles entreprises par année et des milliards d’investissements, regorge de projets divers.</p>
-                        <p class="is-medium">Cette province canadienne possède un écosystème riche, varié et prometteur. Toutefois, bien que des milliers d’entreprises naissent chaque mois, il reste difficile de recenser tous les détails et les projets spécifiques de ces entrepreneurs. Aucune plateforme unique et publique ne permet d’avoir l’ensemble de ces données, spécifiques à l’Alberta.</p>
+                        <p class="is-lead mb-20">{{ $t('project.start-alberta.header-1') }}</p>
+                        <p class="is-medium">{{ $t('project.start-alberta.subheader-1') }}</p>
                     </div>
                 </div>
             </div>
@@ -68,10 +68,10 @@
 
                 <div class="columns is-variable is-8">
                     <div class="column">
-                        <p class="is-medium">En collaboration avec le client, l’équipe Witify a dû initier une première phase pilote ayant pour objectif de regrouper l’ensemble des entrepreneurs, investisseurs et organisations impliquées dans le monde des « Startups » en Alberta. L’équipe s’est penchée sur la vision d’écosystème et de communauté ouverte afin de conceptualiser le produit et d’y intégrer l’ensemble des fonctionnalités qui permettraient autant de communiquer du contenu pertinent, d’intégrer des nouvelles données et d’engager ses utilisateurs.</p>
+                        <p class="is-medium">{{ $t('project.start-alberta.approach-1') }}</p>
                     </div>
                     <div class="column">
-                        <p class="is-medium">Il a été décidé d’y aller en plusieurs phases successives afin de jauger l’intérêt de la communauté et de s’adapter continuellement aux commentaires des utilisateurs.</p>
+                        <p class="is-medium">{{ $t('project.start-alberta.approach-2') }}</p>
                     </div>
                 </div>
             </div>
@@ -86,7 +86,7 @@
         <section class="hero is-quote is-custom-application">
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <h2 class="title">Start Alberta est une ressource <strong>accessible au public</strong>, permettant le <strong>partage</strong> d’histoires de succès, d’innovation et de détermination, dans le monde de l’entreprenariat en Alberta.</h2>
+                    <h2 class="title" v-html="$t('project.start-alberta.header-2')"></h2>
                 </div>
             </div>
         </section>
@@ -100,9 +100,9 @@
         <section class="diptych is-border-bottom">
             <div class="container">
                 <div class="diptych-body">
-                    <h3 class="title">Phase 1 : Produit minimal viable</h3>
-                    <p class="is-medium mb-20">La version « Produit Minimum Viable » de StartAlberta devait rapidement pouvoir intégrer une structure flexible et relativement complexe de données entre les diverses entités de l’écosystème : des individus, des organisations, des investisseurs, des compagnies. De plus, il devait être possible de pouvoir enregistrer et communiquer de l’information de façon simple pour chacune de ces entités.</p>
-                    <p class="is-medium">L’équipe a développé des modèles flexibles permettant d’accomplir ses objectifs, en plus de leur permettre de connecter entre eux, à la manière d’un réseau social. Chaque utilisateur peut s’enregistrer sans peine et un processus fiable et sécuritaire a été implémenté pour supporter ce besoin. Des outils d’administrateur ont aussi été bâtis afin d’offrir une gestion autonome de la plateforme. Grâce à celle-ci, l’administrateur peut gérer les blogues, les différentes données et assurer la viabilité de l’information.</p>
+                    <h3 class="title">Phase 1 : {{ $t('project.start-alberta.mvp') }}</h3>
+                    <p class="is-medium mb-20">{{ $t('project.start-alberta.phase-1-paragraph-1') }}</p>
+                    <p class="is-medium">{{ $t('project.start-alberta.phase-1-paragraph-2') }}</p>
                 </div>
             </div>
             <div class="diptych-image pt-50">
@@ -113,9 +113,9 @@
         <section class="diptych is-border-bottom">
             <div class="container">
                 <div class="diptych-body">
-                    <h3 class="title">Phase 2 : Raffinement</h3>
-                    <p class="is-medium mb-20">Bien que la version pilote remplissait les objectifs de base au niveau « information », celle-ci servait principalement à la validation de la pertinence de la plateforme. Ainsi, lors du développement de la version 1.0, l’équipe de design a pu raffiner la présentation visuelle et la façon de communiquer graphiquement les diverses informations. </p>
-                    <p class="is-medium">En se dédiant à améliorer l’expérience-utilisateur, l’équipe a amélioré la structure des pages, le processus d’inscription et ajouter plusieurs fonctionnalités permettant de naviguer aisément à travers la plateforme et d’augmenter l’engagement des utilisateurs. De plus, plusieurs outils ont été ajoutés pour l’administration, tels que des rapports complets d’information, des outils de communication de masse et autre, dans le but de faciliter sa gestion de la plateforme.</p>
+                    <h3 class="title">Phase 2 : {{ $t('project.start-alberta.refinement') }}</h3>
+                    <p class="is-medium mb-20">{{ $t('project.start-alberta.phase-2-paragraph-1') }}</p>
+                    <p class="is-medium">{{ $t('project.start-alberta.phase-2-paragraph-2') }}</p>
                 </div>
             </div>
             <div class="diptych-image pt-50">
@@ -126,24 +126,24 @@
         <section class="section is-medium">
             <div class="container">
                 <h2 class="title is-size-2">{{ $t('project.general.results') }}</h2>
-                <p class="is-medium mb-50">Start Alberta a mis en ligne sa première version pilote le 4 décembre 2017, limité à une liste sélecte de beta testeurs, et sa première version complète, dédiée au grand public, le 10 juin 2018. L’association entre « Alberta Enterprise » et « Venture Capital Association of Alberta » a permis de rapidement faire naître une communauté diverse sur la plateforme web.</p>
+                <p class="is-medium mb-50">{{ $t('project.start-alberta.results') }}</p>
 
                 <div class="columns">
                     <div class="column">
                         <div class="is-size-2 has-text-weight-semibold">100+</div>
-                        <span class="has-text-grey is-size-5">Nouvelles</span>
+                        <span class="has-text-grey is-size-5">{{ $t('project.start-alberta.news') }}</span>
                     </div>
                     <div class="column">
                         <div class="is-size-2 has-text-weight-semibold">250+</div>
-                        <span class="has-text-grey is-size-5">Compagnies</span>
+                        <span class="has-text-grey is-size-5">{{ $t('project.start-alberta.companies') }}</span>
                     </div>
                     <div class="column">
                         <div class="is-size-2 has-text-weight-semibold">20 000+</div>
-                        <span class="has-text-grey is-size-5">Visites</span>
+                        <span class="has-text-grey is-size-5">{{ $t('project.start-alberta.visits') }}</span>
                     </div>
                     <div class="column">
                         <div class="is-size-2 has-text-weight-semibold">100+</div>
-                        <span class="has-text-grey is-size-5">Investisseurs et organisations</span>
+                        <span class="has-text-grey is-size-5">{{ $t('project.start-alberta.investors-organizations') }}</span>
                     </div>
                 </div>
             </div>
