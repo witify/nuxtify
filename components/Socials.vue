755693bdf9b3992b4c60951c7bcafff84d01e74c
Francois Levesque <info@cumuluswebdesign.com>
2018-08-16 18:31:29 -0400

    Add aria label to socials in footer

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="socials">
-        <a href="https://www.facebook.com/witify/">
+        <a href="https://www.facebook.com/witify/" arial-label="facebook">
             <div class="icon">
                 <i class="mdi mdi-facebook"></i>
             </div>
         </a>
-        <a href="https://twitter.com/WitifyInc">
+        <a href="https://twitter.com/WitifyInc" arial-label="twitter">
             <div class="icon">
                 <i class="mdi mdi-twitter"></i>
             </div>
         </a>
-        <a href="https://github.com/Witify">
+        <a href="https://github.com/Witify" arial-label="github">
             <div class="icon">
                 <i class="mdi mdi-github-circle"></i>
             </div>
         </a>
-        <a href="https://dribbble.com/francoislevesque">
+        <a href="https://dribbble.com/francoislevesque" arial-label="dribbble">
             <div class="icon">
                 <i class="mdi mdi-dribbble"></i>
             </div>
         </a>
-        <a href="https://www.linkedin.com/company/witify">
+        <a href="https://www.linkedin.com/company/witify" arial-label="linkedin">
             <div class="icon">
                 <i class="mdi mdi-linkedin"></i>
             </div>
