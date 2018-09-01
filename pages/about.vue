31cec734ba1a433f6063ec50b1ef109ae3bc3628
= <patrick.vigeant@live.ca>
2018-08-31 20:30:00 -0400

    About page translation

--- pages/about.vue
+++ pages/about.vue
@@ -37,23 +37,23 @@
                         <p class="is-medium">{{ $t('pages.about.excellence-text-2') }}</p>
 
                         <div class="mt-50">
-                            <h2 class="title is-size-3 has-text-weight-normal">What makes our products <strong class="has-text-weight-bold">SUPER</strong></h2>
-                            <p class="is-medium mb-50">We combine talent and hard work to provide the best tools and the best results. Our team understands your desire to differentiate itself.</p>
+                            <h2 class="title is-size-3 has-text-weight-normal" v-html="$t('pages.about.products-super')"></h2>
+                            <p class="is-medium mb-50">{{ $t('pages.about.products-super-text') }}</p>
 
-                            <h3 class="title is-size-5 mb-5">Secure</h3>
-                            <p class="is-medium mb-30">All our web applications are protected against the common <strong>cyper-attacks</strong> and apply the best practices in terms of privacy and <strong>data protection</strong>.</p>
+                            <h3 class="title is-size-5 mb-5">{{ $t('pages.about.secure') }}</h3>
+                            <p class="is-medium mb-30" v-html="$t('pages.about.secure-text')"></p>
 
-                            <h3 class="title is-size-5 mb-5">Usable</h3> 
-                            <p class="is-medium mb-30">Each digital product we craft is built to offer the <strong>best user experience</strong> possible. Our interfaces are fast and easy to use and learn.</p>
+                            <h3 class="title is-size-5 mb-5">{{ $t('pages.about.usable') }}</h3> 
+                            <p class="is-medium mb-30" v-html="$t('pages.about.usable-text')"></p>
 
-                            <h3 class="title is-size-5 mb-5">Powerful</h3>
-                            <p class="is-medium mb-30">Every product we build is custom made and specificaly tailored for our client. By switching to a custom solution, many of our clients saw a <strong>hude upgrade in terms of performance, efficiency and usability</strong>.</p>
+                            <h3 class="title is-size-5 mb-5">{{ $t('pages.about.powerful') }}</h3>
+                            <p class="is-medium mb-30" v-html="$t('pages.about.powerful-text')"></p>
                             
-                            <h3 class="title is-size-5 mb-5">Efficient</h3>
-                            <p class="is-medium mb-30">Our products are blazing fast on every plaforms. They are tailored to our clients needs, <strong>making every action faster and easier to learn</strong>.</p>
+                            <h3 class="title is-size-5 mb-5">{{ $t('pages.about.efficient') }}</h3>
+                            <p class="is-medium mb-30" v-html="$t('pages.about.efficient-text')"></p>
                                   
-                            <h3 class="title is-size-5 mb-5">Reliable</h3>
-                            <p class="is-medium mb-30">Each web solution is hosted on entreprise-level servers. Your data is safely stored and duplicated on the cloud. The high quality of our code allows for easy updates and greater reliability.</p>
+                            <h3 class="title is-size-5 mb-5">{{ $t('pages.about.reliable') }}</h3>
+                            <p class="is-medium mb-30" v-html="$t('pages.about.reliable-text')"></p>
 
                         </div>
 
@@ -79,14 +79,14 @@
 
                 <div class="columns is-variable is-8">
                     <div class="column">
-                        <p class="is-lead mb-20">We believe that every success comes from a strong and sincere cooperation.</p>
-                        <p class="is-medium">At witify, we place great importance on teamwork. Whether internally, ensuring a strong self-help dynamic. Or with our clientele, which is built on transparency and accessible communication at all times. We are here, we are humans and we want to build the bridges of a lasting relationship.</p>
+                        <p class="is-lead mb-20">{{ $t('pages.about.collaboration-text-1') }}</p>
+                        <p class="is-medium">{{ $t('pages.about.collaboration-text-2') }}</p>
                     </div>
                     <div class="column">
-                        <p class="is-size-4 is-italic">"Witify is one of the few web agencies that really listens to its customers. My projects have always been completed with care and speed."</p>
+                        <p class="is-size-4 is-italic">"{{ $t('pages.about.collaboration-quote-text') }}"</p>
                         <div class="has-text-right-tablet mt-20">
                             <p class="is-size-3 has-text-weight-semibold" style="line-height: 1">John Doe</p>
-                            <span>CTO at Acme</span>
+                            <span>{{ $t('pages.about.collaboration-quote-author-role') }}</span>
                         </div>
                     </div>
                 </div>
@@ -109,8 +109,8 @@
                         </div>
                     </div>
 
-                    <p class="is-lead mb-20">Our team is bold and innovative. We dare to seize opportunities and turn the challenges that may be in your way into assets.</p>
-                    <p class="is-medium">Each event is an opportunity to push the boundaries of innovation and creativity. We believe that it is important to undertake projects, to overcome obstacles and to constantly aspire to better than the status quo.</p>
+                    <p class="is-lead mb-20">{{ $t('pages.about.boldness-text-1') }}</p>
+                    <p class="is-medium">{{ $t('pages.about.boldness-text-2') }}</p>
 
                 </div>
             </div>
